9c0f79ce7fe071906d910c574363f2e69d1d4456
Sbipul <kumarroy.kb.kb@gmail.com>
2022-06-27 18:51:20 +0600

    first commit

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const tabs = document.querySelectorAll('.tab')
-
-const loginOption = index => {
-    tabs.forEach(each=>{
-        each.classList.add('d-none')
-        tabs[index].classList.remove('d-none')
-    })
-}
